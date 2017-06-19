386f2aa2633949ccba3a0e89f1ae2c5065a1b317
chenzhuokang <chenzhuokang@cvte.com>
2017-06-19 18:57:16 +0800

    add project files

---
+++ web_server/main.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+var data = fs.readFileSync("index.html"); //.toString();
+//console.log(data);
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 80;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(data);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
